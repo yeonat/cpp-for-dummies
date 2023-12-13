0acceedfaaa0a8d9c3fa0df21170c242186d9204
Jonatan <jjonatansosa@gmail.com>
2023-12-12 21:57:58 -0500

    Chapter 1: Writing Your First C++ Program

---
+++ 1_Part_I_Introduction_t_ CPP_Programming_CONVERT/Convert/ConvertProgram/ConvertProgram/Template.cpp
@@ -0,0 +1,21 @@
+//
+// Template - provides a template to be used a the starting point
+//
+// The following include files define the majority of functions
+// that any gives program will need
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int nNumberofArgs, char* pszArgs[])
+{
+
+	// your c++ code starts here
+
+	// wait until user is ready before terminating program
+	// to allow the user to see the program results
+	system("PAUSE");
+	return 0;
+}
